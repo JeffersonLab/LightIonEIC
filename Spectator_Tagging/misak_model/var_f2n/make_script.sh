e77f6cf16bb779664ad7b0669e98921cf509751f
dhiginbotham <doug@jlab.org>
2014-10-16 16:43:38 -0400

    created a Toolbox directory and renamed one directory

--- Spectator_Tagging/misak_model/var_f2n/make_script.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#
-
-touch 'MISAK_DATA_1.OUT'
-touch 'MISAK_DATA_2.OUT'
-touch 'MISAK_DATA_3.OUT'
-gfortran -o semi_eic semi_eic.f
-./semi_eic
--- Spectator_Tagging/misak_model/var_x/make_script.sh
+++
@@ -1,5 +0,0 @@
-# !/bin/bash
-#
-
-gfortran -o semi_eic semi_eic.f
-./semi_eic
