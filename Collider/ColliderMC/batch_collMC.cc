62cc834f9710d4e7b55a08a761bad2f7ac3df6b9
dhiginbotham <doug@jlab.org>
2015-09-21 15:39:22 -0400

    Removing old collider files

--- Collider/ColliderMC/batch_collMC.cc
+++
@@ -1,5 +0,0 @@
-  void batch_collMC()
-  {
-      gROOT->ProcessLine(".L SpectatorMC_collMC.cpp+");
-      gROOT->ProcessLine("mainx()");
-  }
--- Collider/batch_fixMC.cc
+++
@@ -1,5 +0,0 @@
-  void batch_fixMC()
-  {
-      gROOT->ProcessLine(".L SpectatorMC_fixMC.cpp+");
-      gROOT->ProcessLine("mainx()");
-  }
