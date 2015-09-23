cbd663614be57b42ce2fca6ad931e5ca176cd826
Douglas Higinbotahm <dhiginbotham@users.noreply.github.com>
2015-09-22 21:11:19 -0400

    Add GPL

--- Toolbox/EIC-Physics/SpectatorMC.cpp
+++ Toolbox/EIC-Physics/SpectatorMC.cpp
@@ -1,9 +1,12 @@
 /*
  *  SpectatorMC.cpp
  *  
+ *  Copyright (C) 2014 by Charles Hyde
  *
- *  Created by Charles Hyde on 3/4/14.
- *  Copyright 2014. All rights reserved.
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
  *
  */
 
